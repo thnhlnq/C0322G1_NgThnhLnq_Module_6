56691f78c627d7c19d149d130ca974d4839f1058
thnhlnq <ntlonq14@gmail.com>
2022-10-21 09:02:17 +0700

    ..

--- exam_module_6/exam-module6_be/src/main/java/com/example/repository/IPositionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Position;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IPositionRepository extends JpaRepository<Position, Integer> {
-}
